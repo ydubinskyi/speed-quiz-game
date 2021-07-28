8ab2b783732cb48d88df3b2ec9238a1ca9bcd9f1
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2021-07-28 20:56:07 +0200

    add alembic migrations

--- backend/quiz_game_api/models.py
+++ backend/quiz_game_api/models.py
@@ -3,12 +3,10 @@ from sqlalchemy import Column, String, Text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql.sqltypes import Boolean
 
-from .db import Base
+from .db.base import Base
 
 
 class Question(Base):
-    __tablename__ = "questions"
-
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
     content = Column(String)
     is_active = Column(Boolean)
